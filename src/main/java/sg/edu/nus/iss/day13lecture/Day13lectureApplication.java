b02be2a051e2619e5c6140cbf64007be070339f5
your_name <121241559+MahesanE@users.noreply.github.com>
2023-02-08 09:56:03 +0800

    day13

---
+++ src/main/java/sg/edu/nus/iss/day13lecture/Day13lectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day13lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13lectureApplication.class, args);
+	}
+
+}
